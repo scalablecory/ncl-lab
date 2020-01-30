4985d1e57709c09345789d18ab3e3d257485242c
Cory Nelson <phrosty@gmail.com>
2020-01-29 21:59:17 -0800

    WIP

--- RegisteredServerSample/Program.cs
+++ RegisteredServerSample/Program.cs
@@ -4,14 +4,17 @@ using System.Buffers;
 using System.Net;
 using System.Net.Sockets;
 using System.Security.Cryptography;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace RegisteredServerSample
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            int clientIds = 0, serverIds = 0;
+
             using var bufferPool = new RegisteredMemoryPool();
             using var multiplexer = new RegisteredMultiplexer();
 
@@ -20,32 +23,115 @@ namespace RegisteredServerSample
             listenSocket.Bind(new IPEndPoint(IPAddress.Loopback, 0));
             listenSocket.Listen(int.MaxValue);
 
+            await Task.WhenAll(RunAllClientsAsync(), RunListenLoopAsync());
+
+            async Task RunAllClientsAsync()
+            {
+                var clients = new Task[100];
+                for (int i = 0; i < clients.Length; ++i)
+                {
+                    clients[i] = RunEchoClientAsync();
+                }
+
+                try
+                {
+                    await Task.WhenAll(clients);
+
+                }
+                finally
+                {
+                    listenSocket.Dispose();
+                }
+            }
+
             async Task RunEchoClientAsync()
             {
+                int clientId = Interlocked.Increment(ref clientIds);
+
                 using Socket socket = RegisteredSocket.CreateRegisterableSocket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
                 await socket.ConnectAsync(listenSocket.LocalEndPoint);
 
                 using var registeredSocket = new RegisteredSocket(multiplexer, socket);
 
-                async Task<byte[]> RunEchoClientSendAsync()
+                try
                 {
-                    using IMemoryOwner<byte> memoryOwner = bufferPool.Rent();
-                    Memory<byte> memory = memoryOwner.Memory;
-                    RegisteredOperationContext ctx = registeredSocket.CreateOperationContext();
+                    await Task.WhenAll(RunEchoClientSendAsync(), RunEchoClientReceiveAsync());
+                    Console.WriteLine($"C {clientId} finished successfully.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"C {clientId} failed: {ex.Message}");
+                }
 
-                    Random rng = new Random();
-                    using SHA1 sha1 = SHA1.Create();
-
-                    int bytesLeft = 1024 * 1024 * 1024;
-
-                    while (bytesLeft != 0)
+                async Task RunEchoClientSendAsync()
+                {
+                    try
                     {
-                        Memory<byte> buffer = memory.Slice(0, Math.Min(memory.Length, bytesLeft));
-                        rng.NextBytes(buffer.Span);
+                        using IMemoryOwner<byte> memoryOwner = bufferPool.Rent(bufferPool.MaxBufferSize);
+                        Memory<byte> memory = memoryOwner.Memory;
+                        RegisteredOperationContext ctx = registeredSocket.CreateOperationContext();
 
+                        Random rng = new Random(clientId);
+                        int bytesLeft = 1024 * 1024 * 100;
 
-                        await ctx.SendAsync(buffer);
+                        while (bytesLeft != 0)
+                        {
+                            Memory<byte> buffer = memory.Slice(0, Math.Min(memory.Length, bytesLeft));
+                            rng.NextBytes(buffer.Span);
+
+                            while (buffer.Length != 0)
+                            {
+                                int bytesSent = await ctx.SendAsync(buffer);
+                                Console.WriteLine($"C {clientId} sent {bytesSent:N0} bytes.");
+                                buffer = buffer.Slice(bytesSent);
+                            }
+                        }
+
+                        socket.Shutdown(SocketShutdown.Send);
+                    }
+                    catch
+                    {
+                        socket.Dispose();
+                        throw;
+                    }
+                }
+
+                async Task RunEchoClientReceiveAsync()
+                {
+                    try
+                    {
+                        using IMemoryOwner<byte> memoryOwner = bufferPool.Rent();
+                        Memory<byte> memory = memoryOwner.Memory;
+                        RegisteredOperationContext ctx = registeredSocket.CreateOperationContext();
+
+                        Random rng = new Random(clientId);
+                        byte[] rngBuffer = new byte[memory.Length];
+
+                        while (true)
+                        {
+                            int bytesReceived = await ctx.ReceiveAsync(memory);
+
+                            if (bytesReceived == 0)
+                            {
+                                break;
+                            }
+
+                            Console.WriteLine($"C {clientId} received {bytesReceived:N0} bytes.");
+                            rng.NextBytes(rngBuffer.AsSpan(0, bytesReceived));
+
+                            if (!rngBuffer.AsSpan(0, bytesReceived).SequenceEqual(memory.Span.Slice(0, bytesReceived)))
+                            {
+                                throw new Exception($"C {clientId} failed comparison.");
+                            }
+                        }
+
+                        socket.Shutdown(SocketShutdown.Receive);
+                    }
+                    catch
+                    {
+                        socket.Dispose();
+                        throw;
                     }
                 }
             }
@@ -72,32 +158,46 @@ namespace RegisteredServerSample
 
             async Task ProcessEchoConnectionAsync(Socket acceptSocket)
             {
-                using Socket socket = acceptSocket;
-                using var registeredSocket = new RegisteredSocket(multiplexer, socket);
-                using IMemoryOwner<byte> bufferOwner = bufferPool.Rent();
+                int serverId = Interlocked.Increment(ref serverIds);
 
-                RegisteredOperationContext ctx = registeredSocket.CreateOperationContext();
-                Memory<byte> buffer = bufferOwner.Memory;
+                Console.WriteLine($"S {serverId} started.");
 
-                while (true)
+                try
                 {
-                    int bytesReceived = await ctx.ReceiveAsync(buffer);
+                    using Socket socket = acceptSocket;
+                    using var registeredSocket = new RegisteredSocket(multiplexer, socket);
+                    using IMemoryOwner<byte> bufferOwner = bufferPool.Rent();
 
-                    if (bytesReceived == 0)
+                    RegisteredOperationContext ctx = registeredSocket.CreateOperationContext();
+                    Memory<byte> buffer = bufferOwner.Memory;
+
+                    while (true)
                     {
-                        break;
+                        int bytesReceived = await ctx.ReceiveAsync(buffer);
+
+                        if (bytesReceived == 0)
+                        {
+                            break;
+                        }
+
+                        Console.WriteLine($"S {serverId} received {bytesReceived:N0} bytes.");
+
+                        Memory<byte> sendBuffer = buffer.Slice(0, bytesReceived);
+
+                        while (sendBuffer.Length != 0)
+                        {
+                            int bytesSent = await ctx.SendAsync(sendBuffer);
+                            sendBuffer = sendBuffer.Slice(bytesSent);
+                            Console.WriteLine($"S {serverId} sent {bytesSent:N0} bytes.");
+                        }
                     }
 
-                    Memory<byte> sendBuffer = buffer.Slice(0, bytesReceived);
-
-                    while (sendBuffer.Length != 0)
-                    {
-                        int bytesSent = await ctx.SendAsync(sendBuffer);
-                        sendBuffer = sendBuffer.Slice(bytesSent);
-                    }
+                    socket.Shutdown(SocketShutdown.Both);
                 }
-
-                socket.Shutdown(SocketShutdown.Both);
+                catch(Exception ex)
+                {
+                    Console.WriteLine($"S {serverId} failed: {ex.Message}");
+                }
             }
         }
     }
