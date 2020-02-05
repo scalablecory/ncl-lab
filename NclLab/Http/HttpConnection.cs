37514b4fc525d72ab8d62de239363a8f005cb837
Cory Nelson <phrosty@gmail.com>
2020-02-05 14:38:05 -0800

    clarify comments

--- NclLab/Http/HttpConnection.cs
+++ NclLab/Http/HttpConnection.cs
@@ -5,11 +5,15 @@ using System.Threading.Tasks;
 namespace NclLab.Http
 {
     /// <summary>
-    /// Can have many derived types...
-    /// Http11SerializedConnection (one single connection, one request at a time), Http11PooledConnection (use a connection pool of Http11SerializedConnections), Http2Connection...
-    /// As well as fancier things like a connection that auto-negotiates/migrates across differently-versioned or alt-svc sub-connections
+    /// Can have many derived types, and be layered:
+    /// - Http11Connection (one single connection, one request at a time, no pooling)
+    /// - Http11PooledConnection (issue against a pool of Http11Connection),
+    /// - Http2Connection
+    /// - Http3Connection
+    /// - As well as fancier things like a connection that auto-negotiates/migrates across differently-versioned or alt-svc sub-connections
+    /// - ProxiedConnection, etc..
     /// 
-    /// More or less equivalent to current HttpConnectionPool; SocketsHttpHandler would be refactored to use this intead.
+    /// More or less equivalent to current HttpConnectionPool type; SocketsHttpHandler would be refactored to use this intead.
     /// </summary>
     public abstract class HttpConnection : IAsyncDisposable
     {
@@ -19,11 +23,11 @@ namespace NclLab.Http
         public abstract PreparedHeader PrepareHeader(ReadOnlySpan<char> name, ReadOnlySpan<char> value);
 
         /// <summary>
-        /// Opens a pooled HTTP/1.1 connection, a new stream in HTTP/2, etc.
+        /// Opens a new stream... can be pooled HTTP/1.1 connection, a new stream in HTTP/2, etc.
         /// </summary>
-        /// <param name="cancellationToken"></param>
-        /// <returns></returns>
         public abstract ValueTask<HttpRequestStream> CreateNewRequestStreamAsync(CancellationToken cancellationToken);
+
+        public abstract ValueTask<HttpRequestStream> AcceptRequestStream(CancellationToken cancellationToken);
 
         public abstract ValueTask DisposeAsync();
     }
